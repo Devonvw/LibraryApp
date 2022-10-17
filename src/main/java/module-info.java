7b5739f4cff48411f3bed7929c1fcfd1bf9bf943
DevSoft <devonvanw@gmail.com>
2022-10-17 12:20:32 +0200

    Readme

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module com.example.eindopdracht {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-            requires org.kordamp.ikonli.javafx;
-                
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
     opens com.example.eindopdracht to javafx.fxml;
     exports com.example.eindopdracht;
 }
