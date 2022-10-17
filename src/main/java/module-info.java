928ed39ba2ab6633e3923561bb7ccbe3355f1a46
DevSoft <devonvanw@gmail.com>
2022-10-17 12:15:44 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.eindopdracht {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+            requires org.kordamp.ikonli.javafx;
+                
+    opens com.example.eindopdracht to javafx.fxml;
+    exports com.example.eindopdracht;
+}
